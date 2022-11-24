740bf9019e98dcd820fb600b63f94a4b9bcdb2fe
andreeabrindea <andreeabrindea61@gmail.com>
2022-11-25 00:03:38 +0200

    done some improvements

--- String_transformation.go
+++ String_transformation.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"errors"
 	"log"
 	"os"
 	"sort"
@@ -14,28 +15,40 @@ type CSVRecord struct {
 	location string
 }
 
-func createAListOfRecords(data [][]string) []CSVRecord {
+func parse(data [][]string) []CSVRecord {
 	var inputList []CSVRecord
 
 	for i, line := range data {
-		if i > 0 { // omit header line
-			var rec CSVRecord
-			for j, field := range line {
-				if j == 0 {
-					rec.fname = field
-				}
-				if j == 1 {
-					rec.email = field
-				}
-				if j == 2 {
-					rec.location = field
-				}
-			}
-			inputList = append(inputList, rec)
-
+		// omit header line
+		if i == 0 {
+			continue
 		}
+		rec := CSVRecord{strings.TrimSpace(line[0]), strings.TrimSpace(line[1]), strings.TrimSpace(line[2])}
+		inputList = append(inputList, rec)
 	}
 	return inputList
+}
+
+func remove(list []CSVRecord, index int) ([]CSVRecord, error) {
+	if len(list) == index+1 {
+		return nil, errors.New("index out of bounds")
+	}
+	copy(list[index:], list[index+1:])
+	list[len(list)-1] = CSVRecord{}
+	return list[:len(list)-1], nil
+}
+
+func removeDuplicates(list []CSVRecord) ([]CSVRecord, error) {
+	for i := 1; i < len(list); i++ { //removes duplicates
+		if list[i-1].fname == list[i].fname {
+			newList, err := remove(list, i)
+			if err != nil {
+				return nil, err
+			}
+			list = newList
+		}
+	}
+	return list, nil
 }
 
 func main() {
@@ -55,18 +68,15 @@ func main() {
 	}
 	f.Close()
 
-	inputList := createAListOfRecords(data) //creating a list of those records from input.csv
+	inputList := parse(data) //creating a list of those records from input.csv
 
 	sort.Slice(inputList[:], func(i, j int) bool { //sort the records (because in my head seemed easier to remove duplicates later on)
 		return inputList[i].fname < inputList[j].fname
 	})
 
-	for i := 1; i < len(inputList); i++ { //eliminates duplicates
-		if inputList[i-1].fname == inputList[i].fname {
-			copy(inputList[i:], inputList[i+1:])
-			inputList[len(inputList)-1] = CSVRecord{}
-			inputList = inputList[:len(inputList)-1]
-		}
+	newList, err := removeDuplicates(inputList) //sorted and without duplicates
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	outputFile, err := os.Create("output.csv") //create the output.csv file
@@ -78,13 +88,12 @@ func main() {
 
 	var lines [][]string
 	firstLetter := ""
-	for i, v := range inputList {
+	for i, v := range newList {
 		if v.fname[0:1] != firstLetter {
-			firstLetter = v.fname[0:1]              //get the first letter of each row of records
-			firstLetterAndDots := firstLetter + ":" //add that :
-			line := []string{firstLetterAndDots}    //convert firstLetter to a string so it can be written in the output.csv file
-			newLine := []string{}                   //an empty line is needed before each first letter
-			if i != 0 {                             //except the first one
+			firstLetter = v.fname[0:1]          //get the first letter of each row of records
+			line := []string{firstLetter + ":"} //convert firstLetter to a string so it can be written in the output.csv file
+			newLine := []string{}               //an empty line is needed before each first letter
+			if i != 0 {                         //except the first one
 				lines = append(lines, newLine)
 			}
 
@@ -92,12 +101,7 @@ func main() {
 		}
 
 		line := []string{v.fname, v.email, v.location} //convert the records to string so it can be written in the field
-
-		for i := range line {
-			line[i] = strings.ReplaceAll(line[i], " ", "") // needed to do that so the email and location won't have a space before
-		}
 		lines = append(lines, line)
-
 	}
 
 	csvwriter.WriteAll(lines)

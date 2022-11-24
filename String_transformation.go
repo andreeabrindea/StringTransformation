905a8e8fc88904f9392cd4d9e9ce9b0694c91bbd
andreeabrindea <andreeabrindea61@gmail.com>
2022-11-24 19:31:35 +0200

    unwanted quotes at the start of each row

--- String_transformation.go
+++ String_transformation.go
@@ -3,15 +3,39 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
 	"log"
 	"os"
+	"sort"
 )
 
-type CSVRecords struct {
+type CSVRecord struct {
 	fname    string
 	email    string
 	location string
+}
+
+func createOutput(data [][]string) []CSVRecord {
+	var inputList []CSVRecord
+
+	for i, line := range data {
+		if i > 0 { // omit header line
+			var rec CSVRecord
+			for j, field := range line {
+				if j == 0 {
+					rec.fname = field
+				}
+				if j == 1 {
+					rec.email = field
+				}
+				if j == 2 {
+					rec.location = field
+				}
+			}
+			inputList = append(inputList, rec)
+
+		}
+	}
+	return inputList
 }
 
 func main() {
@@ -24,18 +48,48 @@ func main() {
 
 	//reading the file
 	reader := csv.NewReader(f)
-
-	for {
-		row, err := reader.Read()
-		if err == io.EOF { // Here it stops reading because EOF is the error returned by Read when no more input is available.
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		//printing line by line
-		fmt.Printf("%+v\n", row)
+	data, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
 	}
-	defer f.Close()
+	f.Close()
+
+	inputList := createOutput(data)
+
+	sort.Slice(inputList[:], func(i, j int) bool {
+		return inputList[i].fname < inputList[j].fname
+	})
+
+	for i := 1; i < len(inputList); i++ {
+		if inputList[i-1].fname == inputList[i].fname {
+			copy(inputList[i:], inputList[i+1:])
+			inputList[len(inputList)-1] = CSVRecord{}
+			inputList = inputList[:len(inputList)-1]
+		}
+	}
+
+	outputFile, err := os.Create("output.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvwriter := csv.NewWriter(outputFile)
+	csvwriter.Comma = ','
+
+	var lines [][]string
+	firstLetter := ""
+	for _, v := range inputList {
+		if v.fname[0:1] != firstLetter {
+			firstLetter = v.fname[0:1]
+			line := []string{fmt.Sprintf("%s:\n", firstLetter)}
+			lines = append(lines, line)
+		}
+		line := []string{fmt.Sprintf("%s %s %s\n", v.fname, v.email, v.location)}
+		lines = append(lines, line)
+	}
+
+	csvwriter.WriteAll(lines)
+
+	csvwriter.Flush()
+	outputFile.Close()
 }
